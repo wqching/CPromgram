6da93fe65d3aeb7910d77e4c1109ad1cbb34d6f7
陈文强 <wqching@126.com>
2019-05-20 10:26:40 +0800

    上传测试文件

---
+++ CTest/CTest/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CTest
+//
+//  Created by 陈文强 on 2019/5/20.
+//  Copyright © 2019 陈文强. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
